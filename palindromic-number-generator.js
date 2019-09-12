b72997eaa45fdef26aa6305b347b41cceea72f7b
Arthur <kingart808@gmail.com>
2019-09-11 18:50:47 -1000

    FINISHED!!! WOOT WOOT!

--- palindromic-number-generator.js
+++ palindromic-number-generator.js
@@ -1,5 +1,17 @@
+let results = {};
+let count = 0
 function palindromicNumberGenerator(num) {
-  // do work here
+  let reverse = parseFloat(num.toString().split('').reverse().join(''))
+  if(num == reverse){
+    results.value = num
+    results.steps = count
+  }else{
+    num = reverse + num
+    count++
+    palindromicNumberGenerator(num)
+  }
+  count = 0
+  return results
 }
 
 module.exports = palindromicNumberGenerator;
